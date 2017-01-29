014635060b131c466509ea0d94364f4fe019923f
Ralf1910 <home@ralf-ehren.de>
2017-01-29 11:06:55 +0100

    Add files via upload

--- KoboldVR200/module.php
+++ KoboldVR200/module.php
@@ -68,6 +68,36 @@ class KoboldVR200 extends IPSModule {
 			$this->SetStatus(104);
 		}
 	}
+
+	/*
+		* VR200 Api.
+		* Helper class to make requests against Kobold API
+		*
+		* PHP port based on https://github.com/kangguru/botvac
+		*
+		* Author: Tom Rosenback tom.rosenback@gmail.com  2016
+		*/
+	private static function requestKobold($url, $payload = array(), $method = "POST", $headers = array()) {
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+			if($method == "POST") {
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+			}
+			$requestHeaders = array(
+				'Accept: application/vnd.neato.nucleo.v1'
+			);
+			if(count($headers) > 0) {
+				$requestHeaders = array_merge($requestHeaders, $headers);
+			}
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $requestHeaders);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			return json_decode($result, true);
+	}
+
 	public function UpdateSerialAndKey() {
 
 	}
@@ -282,32 +312,5 @@ class KoboldVR200 extends IPSModule {
 	}
 
 
-	/*
-	* VR200 Api.
-	* Helper class to make requests against Kobold API
-	*
-	* PHP port based on https://github.com/kangguru/botvac
-	*
-	* Author: Tom Rosenback tom.rosenback@gmail.com  2016
-	*/
-	private static function requestKobold($url, $payload = array(), $method = "POST", $headers = array()) {
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-		if($method == "POST") {
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-		}
-		$requestHeaders = array(
-			'Accept: application/vnd.neato.nucleo.v1'
-		);
-		if(count($headers) > 0) {
-			$requestHeaders = array_merge($requestHeaders, $headers);
-		}
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $requestHeaders);
-		$result = curl_exec($ch);
-		curl_close($ch);
-		return json_decode($result, true);
-	}
+
  }
