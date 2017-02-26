df8a17903dd93cd85a32a34754aa3df5c99c59c6
Ralf1910 <home@ralf-ehren.de>
2017-02-26 16:04:30 +0100

    Add files via upload

--- Stromzaehler/module.php
+++ Stromzaehler/module.php
@@ -56,7 +56,7 @@ class Stromzaehler extends IPSModule {
 
 	public function UpdateStromzaehler() {
 
-		SetValue($this->GetIDForIdent("currentPower"), getValueFloat($this->ReadPropertyInteger("EKMCurrentObjektID"));
+		SetValue($this->GetIDForIdent("currentPower"), getValueFloat($this->ReadPropertyInteger("EKMCurrentObjektID")));
 
 		if ((GetValueInteger($this->ReadPropertyInteger("EKMCounterObjektID")) + GetValueInteger($OffsetObjektID))>= GetValueInteger(28348 /*[Geräte\KG\Waschkeller\EKM-868 128:1 (Haushalt)\Counter Persistent]*/ )) {
  			SetValueInteger (28348 /*[Geräte\KG\Waschkeller\EKM-868 128:1 (Haushalt)\Counter Persistent]*/, GetValueInteger(49624 /*[Geräte\KG\Waschkeller\EKM-868 128:1 (Haushalt)\Counter]*/) + GetValueInteger(38863 /*[Geräte\KG\Waschkeller\EKM-868 128:1 (Haushalt)\Offset]*/));
