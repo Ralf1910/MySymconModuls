917c36aba395beb8873288defdd8c2a6e751ca6f
Ralf1910 <home@ralf-ehren.de>
2017-03-01 21:30:24 +0100

    Add files via upload

--- Batterie/module.php
+++ Batterie/module.php
@@ -30,8 +30,8 @@ class Batterie extends IPSModule {
 		// Variablen anlegen
 		$this->RegisterVariableFloat("fuellstand", "Füllstand", "~Electricity", 10);
 		$this->RegisterVariableInteger("aktuelleLadeleistung", "aktuelle Ladeleistung", "Power.Watt", 20);
-		$this->RegisterVariableInteger("aktuelleEinspeisung", "aktuelle Einspeisung", "Power.Watt", 40);
-		$this->RegisterVariableFloat("eingespeisteEnergie", "Eingepeiste Energie", "~Electricity", 30);
+		$this->RegisterVariableInteger("aktuelleEinspeisung", "aktuelle Einspeisung", "Power.Watt", 30);
+		$this->RegisterVariableFloat("eingespeisteEnergie", "Eingepeiste Energie", "~Electricity", 40);
 		$this->RegisterVariableInteger("aktuellerNetzbezug", "aktueller Netzbezug", "Power.Watt", 50);
 		$this->RegisterVariableFloat("bezogeneEnergie", "Bezogene Energie", "~Electricity", 60);
 		$this->RegisterVariableFloat("rollierendeZyklen", "Rollierende Zyklen pro Jahr", "", 70);
